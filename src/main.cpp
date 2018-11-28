c64b488628e362423873343f2fe5e02ba93e1559
Sam Meech-Ward <sam@meech-ward.com>
2018-11-28 15:17:28 -0800

    Sync the time when the arduino is started

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#include "time_serial.h"
+
 void setup() {
-  // put your setup code here, to run once:
+  TimeSerial::setup();
+  TimeSerial::requestTime();
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  TimeSerial::syncTime();
+
+  delay(500);
+
+  if (TimeSerial::timeIsSet()) {
+    Serial.println(year());
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+   delay(500);  
 }
